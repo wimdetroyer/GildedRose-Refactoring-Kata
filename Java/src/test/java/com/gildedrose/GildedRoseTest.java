483a08eea5cc8600ddff79fd41ef33a2c9df00b9
Wim De Troyer <wim.detroyer@partenamut.be>
2022-07-06 13:22:22 +0200

    Setup:
    - Remove rubbish
    - preliminary setup
    - some thoughts on existing & new code

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,11 +7,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class GildedRoseTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    void testSulfarasItemNeverChangesItsValue() {
+        Item[] items = new Item[] {createItem("foo", 0, 0)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals("fixme", app.items[0].name);
+    }
+
+    private Item createItem(String name, int sellIn, int quality) {
+        return new Item(name, sellIn, quality);
     }
 
 }
